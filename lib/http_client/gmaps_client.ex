76e48b7672803428dcb7591d17fc1e9b2606feea
MusH <collinsmucheru@gmail.com>
2019-07-24 07:28:36 +0300

    some logs for the GmapsClient

--- lib/http_client/gmaps_client.ex
+++ lib/http_client/gmaps_client.ex
@@ -1,14 +1,20 @@
 defmodule HttpClient.GmapsClient do
+  require Logger
   use GenServer
   alias HttpClient.Api
   alias HttpClient.Utils
 
   def start_link(config) do
+    Logger.info("GmapsClient starting")
+    Logger.debug("GmapsClient config: #{inspect(config)}")
     GenServer.start_link(__MODULE__, config, name: :gmaps_http_client)
   end
 
   # GenServer Callbacks
-  def init(config), do: {:ok, config}
+  def init(config) do
+    Logger.debug("GmapsClient running")
+    {:ok, config}
+  end
 
   def fetch_distance_matrix(_origin, _destination) do
   end
@@ -21,6 +27,10 @@ defmodule HttpClient.GmapsClient do
   end
 
   def fetch_directions(origin, destination) do
+    Logger.debug("GmapsClient fetch_directions/2
+      origin: #{inspect(origin)}
+      destination: #{inspect(destination)}")
+
     directions_request(%{
       :origin => origin,
       :destination => destination,
@@ -29,9 +39,14 @@ defmodule HttpClient.GmapsClient do
   end
 
   def directions_request(params) do
-    params
-    |> Utils.url(:base_url__directions)
-    |> Api.get()
-    |> Utils.http_resp_ok?()
+    response =
+      params
+      |> Utils.url(:base_url__directions)
+      |> Api.get()
+      |> Utils.http_resp_ok?()
+
+    Logger.debug("GmapsClient directions_request/1 response: #{inspect(response)}")
+
+    response
   end
 end
