f3cabaad921f23f0712dbf5055421e0686c75c00
MusH <collinsmucheru@gmail.com>
2019-11-02 22:21:44 +0300

    change promo_code validity default value

--- priv/repo/migrations/20191101143301_create_promo_codes.exs
+++ priv/repo/migrations/20191101143301_create_promo_codes.exs
@@ -6,7 +6,7 @@ defmodule Promo.Repo.Migrations.CreatePromoCodes do
       add :p_code, :string, null: false
       add :amount, :float, null: false
       add :expiry_date, :date, null: false
-      add :status, :boolean, default: false, null: false
+      add :status, :boolean, default: false, null: true
       add :radius, :float
       add :event_location_id, references(:event_locations, on_delete: :nothing), null: false
 
