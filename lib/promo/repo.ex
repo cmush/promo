8ffdcb1a15fed0f394c1d5a1b1bdbe94f1250d6c
MusH <collinsmucheru@gmail.com>
2019-11-03 16:37:06 +0300

    load DATABASE_URL from env

--- lib/promo/repo.ex
+++ lib/promo/repo.ex
@@ -2,4 +2,9 @@ defmodule Promo.Repo do
   use Ecto.Repo,
     otp_app: :promo,
     adapter: Ecto.Adapters.Postgres
+
+  @spec init(any, keyword) :: {:ok, [{atom, any}, ...]}
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+  end
 end
