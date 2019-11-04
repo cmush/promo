932b8cc4bb3f7b18fb3fc8645d474bfc3624c3e3
MusH <collinsmucheru@gmail.com>
2019-11-04 16:50:30 +0300

    remove old boilerplate, log container startup

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :promo, PromoWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "safepromo.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  # cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: :crypto.strong_rand_bytes(43) |> Base.encode64()
 
 # Do not print debug messages in production
--- start.sh
+++ start.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "PORT"
+echo "PORT=${PORT}"
 export PORT=${PORT}
 
-echo "BASE_URL_DIRECTIONS"
+echo "BASE_URL_DIRECTIONS=${BASE_URL_DIRECTIONS}"
 export BASE_URL_DIRECTIONS=${BASE_URL_DIRECTIONS}
 
-echo "BASE_URL_DISTANCE_MATRIX"
+echo "BASE_URL_DISTANCE_MATRIX=${BASE_URL_DISTANCE_MATRIX}"
 export BASE_URL_DISTANCE_MATRIX=${BASE_URL_DISTANCE_MATRIX}
 
-echo "GMAPS_API_KEY"
+echo "GMAPS_API_KEY=${GMAPS_API_KEY}"
 export GMAPS_API_KEY=${GMAPS_API_KEY}
 
-echo "creating and exporting DATABASE_URL"
+echo "DATABASE_URL=${DATABASE_URL}"
 export DATABASE_URL=${DATABASE_URL}
 
-echo "starting africabeat_umbrella in foreground mode"
+echo "starting promo in foreground mode"
 /opt/app/bin/promo foreground
 
