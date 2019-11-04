320b2b5da278456500035c4b86111ccbbdc768ad
MusH <collinsmucheru@gmail.com>
2019-11-04 14:56:32 +0300

    update start file to use DATABASE_URL as env directly, remove app.json (docker active), mod.gitignore

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ echo "GMAPS_API_KEY"
 export GMAPS_API_KEY=${GMAPS_API_KEY}
 
 echo "creating and exporting DATABASE_URL"
-export DATABASE_URL=postgres://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}
+export DATABASE_URL=${DATABASE_URL}
 
 echo "starting africabeat_umbrella in foreground mode"
 /opt/app/bin/promo foreground
