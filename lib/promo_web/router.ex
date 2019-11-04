55d7d2e1aae8e929f6fc70a8b74c829105a23b73
MusH <collinsmucheru@gmail.com>
2019-11-04 06:44:19 +0300

    install pow

--- lib/promo_web/router.ex
+++ lib/promo_web/router.ex
@@ -1,8 +1,34 @@
 defmodule PromoWeb.Router do
   use PromoWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :protected do
+    plug Pow.Plug.RequireAuthenticated,
+         error_handler: Pow.Phoenix.PlugErrorHandler
+  end
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    pow_routes()
+  end
+
+  scope "/", PromoWeb do
+    pipe_through [:browser, :protected]
+
+    # Add your protected routes here
   end
 
   scope "/api", PromoWeb do
